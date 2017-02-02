5af5d00bfba0fd372a335cd6b734b7d80640e2d1
Cory House <housecor@gmail.com>
2017-02-02 15:54:35 -0600

    Add Storyshots. Dumping CRA

---
+++ src/stories/storyshots.test.js
@@ -0,0 +1,5 @@
+// This file uses storyshots to generate Jest
+// snapshot tests for all the stories that we've
+// created in React Storybook.
+import initStoryshots from 'storyshots';
+initStoryshots();
