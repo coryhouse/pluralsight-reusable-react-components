b809c6a5429c71c1626c2e898c40e2fe912d6f56
Cory House <housecor@gmail.com>
2017-02-07 21:22:19 -0600

    Separate doc pages

--- docs/src/app/components/Docs.js
+++ docs/src/app/components/Docs.js
@@ -1,33 +1,32 @@
 import React from 'react';
-import PropDocumentation from './PropDocumentation';
 import Title from 'react-title-component';
-import progressBarCode from '!raw!pluralsight-ui/ProgressBar/ProgressBar';
-import Example10Percent from './ProgressBar/Example10Percent';
-import Example10PercentCode from '!raw!./ProgressBar/Example10Percent';
-import Example70Percent from './ProgressBar/Example70Percent';
-import Example70PercentCode from '!raw!./ProgressBar/Example70Percent';
-import Example100Percent from './ProgressBar/Example100Percent';
-import Example100PercentCode from '!raw!./ProgressBar/Example100Percent';
-import Example from './Example';
 import {parse} from 'react-docgen';
-
-const progressBarInfo = parse(progressBarCode);
+import ProgressBarPage from './ProgressBar/ProgressBarPage';
 
 export default class Docs extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      page: ''
+    };
+  }
+
+  loadPage(page) {
+    this.setState({page: page});
+  }
+
   render() {
     return (
       <div>
-        <Title render={(previousTitle) => `Pluralsight UI Docs`} />
-        <h2>ProgressBar</h2>
-        <p>{progressBarInfo.description}</p>
-        <h3>Examples</h3>
+        <div style={{float: 'left', width: 200}}>
+          <a href="#" onClick={() => loadPage('ProgressBar')}>ProgressBar</a>
+        </div>
 
-        <Example code={Example10PercentCode}><Example10Percent /></Example>
-        <Example code={Example70PercentCode}><Example70Percent /></Example>
-        <Example code={Example100PercentCode}><Example100Percent /></Example>
-
-        <h3>Props</h3>
-        <PropDocumentation props={progressBarInfo.props} />
+        <div style={{float: 'left'}}>
+          {this.page == '' ? <ProgressBarPage/> : <ProgressBarPage/>}
+        </div>
+        <div style={{clear:'both'}}></div>
       </div>
     )
   }
