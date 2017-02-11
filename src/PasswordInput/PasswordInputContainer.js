9a8ba8dc8bee20dff0070ca1da583d039a3d3544
Cory House <housecor@gmail.com>
2017-02-11 10:50:02 -0600

    Add back to fix storybook

---
+++ src/PasswordInput/PasswordInputContainer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PasswordInput from './PasswordInput';
+
+class PasswordInputContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      password: ''
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <PasswordInput
+          name="password"
+          onChange={(event) => this.setState({ password: event.target.value })}
+          value={this.state.password}
+          {...this.props} />
+      </div>
+    )
+  }
+}
+
+export default PasswordInputContainer;
