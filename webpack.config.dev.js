c38d446af13f0005f8c3bc5de5e145d3aa8313e1
Cory House <housecor@gmail.com>
2017-02-27 05:12:29 -0600

    Missing semicolons

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,5 +1,5 @@
-var path = require('path')
-var webpack = require('webpack')
+var path = require('path');
+var webpack = require('webpack');
 const buildPath = path.resolve(__dirname, 'src/www');
 
 module.exports = {
